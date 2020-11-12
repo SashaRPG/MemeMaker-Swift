4eec1415f3aa1261a7b6bfcc0e619c605b9a8195
SashaRPG <uasasharpg@gmail.com>
2020-11-12 19:13:38 +0300

    Initial Commit

---
+++ Meme Maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme Maker
+//
+//  Created by ALEXANDER KOTLUBEEV on 12/11/2020.
+//  Copyright © 2020 ALEXANDER KOTLUBEEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
