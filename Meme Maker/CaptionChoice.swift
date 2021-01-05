bd5b03fc590797ea2554e61fc2df3033ad4b330c
SashaRPG <uasasharpg@gmail.com>
2021-01-05 18:55:02 +0300

    created CaptionChoice.swift

---
+++ Meme Maker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  Meme Maker
+//
+//  Created by ALEXANDER KOTLUBEEV on 05/01/2021.
+//  Copyright © 2021 ALEXANDER KOTLUBEEV. All rights reserved.
+//
+
+import Foundation
+
+struct CaptionChoice{
+    var emoji : String;
+    var caption : String;
+}
