61803e5d5de4f68ebd7054ba84ad74c0a73ad272
SashaRPG <uasasharpg@gmail.com>
2021-01-05 21:10:18 +0300

    Almost finished MemeMaker project from "Into to App Development with Swift" Needs more images to choose from.

--- Meme Maker/CaptionChoice.swift
+++ Meme Maker/CaptionChoice.swift
@@ -11,4 +11,5 @@ import Foundation
 struct CaptionChoice{
     var emoji : String;
     var caption : String;
+    var imageName : String;
 }
--- Meme Maker/ViewController.swift
+++ Meme Maker/ViewController.swift
@@ -13,11 +13,70 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureTopCaption();
+        configureBottomCaption();
+        initLabels();
     }
-    @IBOutlet weak var topCaptionSegmentedControl: UISegmentedControl!
     
+    var topChoices = [CaptionChoice]();
+    var bottomChoices = [CaptionChoice]();
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var topCaptionSegmentedControl: UISegmentedControl!
     @IBOutlet weak var bottomCaptionSegmentedControl: UISegmentedControl!
-    @IBAction func segmentedControls(_ sender: Any) {
+   
+    
+//    let topChoices = [];
+//    let bottomChoices = [];
+    func configureTopCaption(){
+        topCaptionSegmentedControl.removeAllSegments();
+        topCaptionSegmentedControl.selectedSegmentIndex = 0;
+        let option1 = CaptionChoice(emoji: "🕶", caption: "You know what's cool?", imageName: "kitty");
+        let option2 = CaptionChoice(emoji: "💥", caption: "You know what makes me mad?", imageName: "doge");
+        let option3 = CaptionChoice(emoji: "💕", caption: "You know what I love?", imageName: "monkey");
+        topChoices = [option1, option2, option3];
+        
+        for choice in topChoices{
+            topCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
+      
+    }
+    
+    func configureBottomCaption(){
+        bottomCaptionSegmentedControl.removeAllSegments();
+        bottomCaptionSegmentedControl.selectedSegmentIndex = 0;
+        let option1 = CaptionChoice(emoji: "😼", caption: "Cats wearing hats", imageName: "kitty");
+        let option2 = CaptionChoice(emoji: "🐕", caption: "Dogs carrying logs", imageName: "doge");
+        let option3 = CaptionChoice(emoji: "🐒", caption: "Monkey being funky", imageName: "monkey");
+        bottomChoices = [option1, option2, option3];
+        
+        for choice in bottomChoices{
+            bottomCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false)
+        }
+      
+    }
+    
+    func initLabels(){
+        topCaptionSegmentedControl.selectedSegmentIndex = 0;
+        bottomCaptionSegmentedControl.selectedSegmentIndex = 0;
+        topCaptionLabel.text = topChoices.first?.caption;
+        bottomCaptionLabel.text = bottomChoices.first?.caption;
+        mainImageView.image = UIImage(named: topChoices[0].imageName)
+    }
+    
+    @IBAction func topChoiceControls(_ sender: UISegmentedControl) {
+        let currentChoice = topChoices[sender.selectedSegmentIndex];
+        topCaptionLabel.text = currentChoice.caption;
+        mainImageView.image = UIImage(named: currentChoice.imageName);
+        
+    }
+    
+    @IBAction func bottomChoiceControls(_ sender: UISegmentedControl) {
+        let currentChoice = bottomChoices[sender.selectedSegmentIndex];
+        bottomCaptionLabel.text = currentChoice.caption;
+        mainImageView.image = UIImage(named: currentChoice.imageName);
     }
     
 }
